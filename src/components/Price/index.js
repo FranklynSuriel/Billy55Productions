a26da4be1dbc051baea4760bcbcfd576fed37376
Franksu <fsuriel@gmail.com>
2023-09-20 12:24:16 -0400

    Added more styles and changed prices to dominican Pesos

--- src/components/Price/index.js
+++ src/components/Price/index.js
@@ -7,7 +7,7 @@ function Price() {
       <div className="card_container">
         <div className="card">
           <h4>Basico</h4>
-          <h2> 99.99</h2>
+          <h2> 3,099.99</h2>
           <p>✓ 7 Fotos</p>
           <p>✓ Galleria de Fotos </p>
           <p>✓ 1 Locacion</p>
@@ -15,7 +15,7 @@ function Price() {
         </div>
         <div className="card">
           <h4>Avanzado</h4>
-          <h2> 199.99</h2>
+          <h2> 6,199.99</h2>
           <p>✓ 15 Photos</p>
           <p>✓ Album</p>
           <p>✓ 2 Locaciones</p>
@@ -23,10 +23,10 @@ function Price() {
         </div>
         <div className="card">
           <h4>Premium</h4>
-          <h2> 299.99</h2>
+          <h2> 9,299.99</h2>
           <p>✓ 25 Fotos</p>
-          <p>✓ fotos con Drones</p>
-          <p>✓ diferentes Outfit</p>
+          <p>✓ Fotos con Drones</p>
+          <p>✓ Diferentes Outfit</p>
           <p>✓ Montaje de video</p>
         </div>
       </div>
