dd3a792f1547a8b74cde115c9d58be8ea299bb16
Franklyn <118221385+FranklynSuriel@users.noreply.github.com>
2024-12-21 16:26:54 -0500

    Update index.js
    Updated link

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -54,7 +54,7 @@ function Hero() {
             id="nav-button"
             onClick={() => {
               window.location.href =
-                "https://www.billy55producciones.com";
+                "https://www.billy55producciones.com/login.php";
             }}
           >
             {translate("navLogin")}
